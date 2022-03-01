6c0f254e809d520734984b965d341648a32e6259
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-03-01 15:53:15 +0200

    i gave up on fixing a bug

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const nav = document.querySelector(".primary-navigation");
-const navButton = document.querySelector(".nav-toggle");
+const navToggle = document.querySelector(".mobile-nav-toggle");
 
-navButton.addEventListener("click", () => {
-  const visiblity = nav.getAttribute("data-visible");
-  const expanded = navButton.setAttribute("aria-expanded", true)
-  if (visiblity === "false") {
-    nav.setAttribute("data-visible", true);
-    navButton.setAttribute("aria-expanded", true)
-  } else if (visiblity === "true") {
-    nav.setAttribute("data-visible", false);
-    navButton.setAttribute("aria-expanded", false)
-  } else {
-    console.log("data-visible or aria-expanded attribute is unknown", visiblity, expanded );
-  }
-});
+navToggle.addEventListener("click", () => {
+    
+    const visiblity = nav.getAttribute("data-visible");
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+})
