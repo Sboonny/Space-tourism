cea15d0e2c2abb2d45a9828064f147aad0504a40
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-03-03 18:21:41 +0200

    fixed a typo

--- tabs.js
+++ tabs.js
@@ -3,22 +3,26 @@ const tabs = tabList.querySelectorAll('[role="tab"]');
 
 tabList.addEventListener("keydown", changeTabFocus);
 
+for (const tab of tabs) {
+  tab.addEventListener("click", changeTabPanel);
+}
+
 let focusedTab = 0;
 function changeTabFocus(event) {
   const keydownLeft = 37;
   const keydownRight = 39;
 
-  if (event.keycode === keydownRight || event.keycode === keydownLeft) {
+  if (event.keyCode === keydownRight || event.keyCode === keydownLeft) {
     tabs[focusedTab].setAttribute("tabindex", -1);
   }
 
-  if (event.keycode === keydownRight) {
+  if (event.keyCode === keydownRight) {
     focusedTab++;
     if (focusedTab >= tabs.length) {
       focusedTab = 0;
     }
   }
-  if (event.keycode === keydownLeft) {
+  if (event.keyCode === keydownLeft) {
     focusedTab--;
     if (focusedTab < 0) {
       focusedTab = tabs.length - 1;
@@ -27,4 +31,9 @@ function changeTabFocus(event) {
 
   tabs[focusedTab].setAttribute("tabindex", 0);
   tabs[focusedTab].focus();
+}
+
+function changeTabPanel (event) {
+  const targetedTab = event.target;
+  const tabPanel = targetedTab.getAttribute()
 }
